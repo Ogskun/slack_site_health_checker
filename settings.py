3cdee35e589c082f9e9b5cfa83f055a3e52fa382
Jayson Ogso <jayson.ogso@tondosolutions.com>
2024-02-27 14:13:29 +0800

    update requirements, add comments

--- settings.py
+++ settings.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Slack app token
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+
+# Channel name which is connected to your app
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL')
 
+# List of site urls
 APPS = [{
     # 'name': '',
     # 'url': '',
